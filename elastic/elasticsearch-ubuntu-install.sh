c83aaaa660a2dd80ac2d9b540bc6800a948e100a
Martijn Laarman <mpdreamz@gmail.com>
2015-11-11 13:53:07 +0100

    log output of java instalation

--- elastic/elasticsearch-ubuntu-install.sh
+++ elastic/elasticsearch-ubuntu-install.sh
@@ -217,7 +217,9 @@ install_java()
     apt-get -y update  > /dev/null
     echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
     echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-    apt-get -y install oracle-java8-installer  > /dev/null
+    apt-get -y install oracle-java8-installer 
+
+    log "Installed Java"
 }
 
 # Install Elasticsearch
