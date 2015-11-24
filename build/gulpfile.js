80c2e88e9a81dcbdef8fbc08649f61dc248dc79c
Martijn Laarman <mpdreamz@gmail.com>
2015-11-24 16:41:06 +0100

    npm install now calls build, build zips to dist using dateformatted name

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,22 +1,26 @@
-var gulp = require('gulp');
-var eclint = require('eclint');
+var gulp = require("gulp");
+var eclint = require("eclint");
 var jsonlint = require("gulp-jsonlint");
+var zip = require("gulp-zip");
+var dateFormat = require("dateformat");
 
-gulp.task('default', function() {
+gulp.task("default", function() {
     var stream = gulp.src([
-            '../src/**/*.json'
+            "../src/**/*.json"
         ])
         .pipe(jsonlint())
         .pipe(jsonlint.reporter())
         .pipe(eclint.check({
             reporter: function(file, message) {
-                //var relativePath = path.relative('.', file.path);
-                console.error(file.path + ':', message);
+                //var relativePath = path.relative(".", file.path);
+                console.error(file.path + ":", message);
             }
         }))
         .pipe(jsonlint.failAfterError())
+        .pipe(zip("elasticsearch-marketplace" + dateFormat(new Date(), "-yyyymmdd-hhMMss-Z").replace("+","-") +".zip"))
+        .pipe(gulp.dest("../dist"));
         ;
-    stream.on('finish', function() {
+    stream.on("finish", function() {
     });
     return stream;
 });
