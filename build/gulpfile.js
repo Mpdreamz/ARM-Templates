9f52a3041e5d2744c507969bd04e16b860f4e54d
Martijn Laarman <mpdreamz@gmail.com>
2015-11-24 16:21:37 +0100

    npm run build now validates editorconfig settings and json lints all json files, failing if they contain an error

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,25 +1,22 @@
 var gulp = require('gulp');
 var eclint = require('eclint');
-
-
+var jsonlint = require("gulp-jsonlint");
 
 gulp.task('default', function() {
-    var hasErrors = false;
     var stream = gulp.src([
-            '*',
             '../src/**/*.json'
         ])
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter())
         .pipe(eclint.check({
             reporter: function(file, message) {
-                hasErrors = true;
                 //var relativePath = path.relative('.', file.path);
                 console.error(file.path + ':', message);
             }
-        }));
+        }))
+        .pipe(jsonlint.failAfterError())
+        ;
     stream.on('finish', function() {
-        if (hasErrors) {
-            process.exit(1);
-        }
     });
     return stream;
 });
